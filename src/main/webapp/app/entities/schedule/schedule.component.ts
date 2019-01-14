b783893681a47d43adf46a0f381a8219964a6c2b
Tom Cory <tom@cory-online.de>
2019-01-14 20:55:15 +0100

    added schedule entity scaffold and translation files

--- src/main/webapp/app/entities/schedule/schedule.component.ts
+++ src/main/webapp/app/entities/schedule/schedule.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { JhiAlertService, JhiEventManager, JhiParseLinks } from 'ng-jhipster';
+import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 import { Principal } from 'app/core';
+import { ScheduleService } from './schedule.service';
+import { IShift } from 'app/shared/model/shift.model';
 
 @Component({
     selector: 'jhi-schedule',
@@ -10,12 +14,26 @@ import { Principal } from 'app/core';
     styleUrls: ['./schedule.css']
 })
 export class ScheduleComponent implements OnInit, OnDestroy {
+    currentAccount: any;
+    shifts: IShift[];
     error: any;
     success: any;
+    eventSubscriber: Subscription;
+    currentSearch: string;
+    routeData: any;
+    links: any;
+    totalItems: any;
+    queryCount: any;
+    itemsPerPage: any;
+    page: any;
+    predicate: any;
+    previousPage: any;
+    reverse: any;
 
     chart = [];
 
     constructor(
+        private scheduleService: ScheduleService,
         private parseLinks: JhiParseLinks,
         private jhiAlertService: JhiAlertService,
         private principal: Principal,
@@ -24,9 +42,116 @@ export class ScheduleComponent implements OnInit, OnDestroy {
         private eventManager: JhiEventManager
     ) {}
 
-    ngOnInit() {}
+    loadAll() {
+        if (this.currentSearch) {
+            this.scheduleService
+                .search({
+                    page: this.page - 1,
+                    query: this.currentSearch,
+                    size: this.itemsPerPage,
+                    sort: this.sort()
+                })
+                .subscribe(
+                    (res: HttpResponse<IShift[]>) => this.paginateShifts(res.body, res.headers),
+                    (res: HttpErrorResponse) => this.onError(res.message)
+                );
+            return;
+        }
+        this.scheduleService
+            .query({
+                page: this.page - 1,
+                size: this.itemsPerPage,
+                sort: this.sort()
+            })
+            .subscribe(
+                (res: HttpResponse<IShift[]>) => this.paginateShifts(res.body, res.headers),
+                (res: HttpErrorResponse) => this.onError(res.message)
+            );
+    }
 
-    ngOnDestroy() {}
+    loadPage(page: number) {
+        if (page !== this.previousPage) {
+            this.previousPage = page;
+            this.transition();
+        }
+    }
+
+    transition() {
+        this.router.navigate(['/shift'], {
+            queryParams: {
+                page: this.page,
+                size: this.itemsPerPage,
+                search: this.currentSearch,
+                sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+            }
+        });
+        this.loadAll();
+    }
+
+    clear() {
+        this.page = 0;
+        this.currentSearch = '';
+        this.router.navigate([
+            '/shift',
+            {
+                page: this.page,
+                sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+            }
+        ]);
+        this.loadAll();
+    }
+
+    search(query) {
+        if (!query) {
+            return this.clear();
+        }
+        this.page = 0;
+        this.currentSearch = query;
+        this.router.navigate([
+            '/shift',
+            {
+                search: this.currentSearch,
+                page: this.page,
+                sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+            }
+        ]);
+        this.loadAll();
+    }
+
+    ngOnInit() {
+        this.loadAll();
+        this.principal.identity().then(account => {
+            this.currentAccount = account;
+        });
+        this.registerChangeInShifts();
+    }
+
+    ngOnDestroy() {
+        this.eventManager.destroy(this.eventSubscriber);
+    }
+
+    trackId(index: number, item: IShift) {
+        return item.id;
+    }
+
+    registerChangeInShifts() {
+        this.eventSubscriber = this.eventManager.subscribe('shiftListModification', response => this.loadAll());
+    }
+
+    sort() {
+        const result = [this.predicate + ',' + (this.reverse ? 'asc' : 'desc')];
+        if (this.predicate !== 'id') {
+            result.push('id');
+        }
+        return result;
+    }
+
+    private paginateShifts(data: IShift[], headers: HttpHeaders) {
+        this.links = this.parseLinks.parse(headers.get('link'));
+        this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
+        this.queryCount = this.totalItems;
+        this.shifts = data;
+    }
 
     private onError(errorMessage: string) {
         this.jhiAlertService.error(errorMessage, null, null);
