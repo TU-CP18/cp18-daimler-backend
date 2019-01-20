706f340210bc309223a67f9cbbf06a923d1e78d2
Tom Cory <tom@cory-online.de>
2019-01-20 18:50:02 +0100

    #63 added remote API data binding to schedule (first iteration, still needs work)

--- src/main/webapp/app/entities/schedule/schedule.component.ts
+++ src/main/webapp/app/entities/schedule/schedule.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, Inject, ViewChild, ViewEncapsulation, OnInit, OnDestroy } from '@angular/core';
 import { extend, isNullOrUndefined, Browser } from '@syncfusion/ej2-base';
 import {
     ScheduleComponent,
@@ -17,13 +17,30 @@ import {
 } from '@syncfusion/ej2-angular-schedule';
 import { shiftData } from './datasource';
 
+import { ScheduleService } from './schedule.service';
+import { SafetyDriverService } from './../safety-driver/safety-driver.service';
+
+import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { JhiEventManager, JhiParseLinks, JhiAlertService } from 'ng-jhipster';
+
+import { IShift } from 'app/shared/model/shift.model';
+import { ScheduleShift, IScheduleShift } from 'app/shared/model/schedule-shift.model';
+import { ScheduleDriver, IScheduleDriver } from 'app/shared/model/schedule-driver.model';
+import { ISafetyDriver } from 'app/shared/model/safety-driver.model';
+
+import { Principal } from 'app/core';
+import { EventSettings } from '@syncfusion/ej2-schedule/src/schedule/models/event-settings';
+
 @Component({
     selector: 'jhi-schedule',
     templateUrl: './schedule.component.html',
     styleUrls: ['./schedule.css', 'material.css'],
     encapsulation: ViewEncapsulation.None
 })
-export class CPScheduleComponent {
+export class CPScheduleComponent implements OnInit, OnDestroy {
+    // BEGIN SYNCFUSION FIELDS
     public selectedDate: Date = new Date(2018, 7, 1);
     public timeScale: TimeScaleModel = { interval: 60, slotCount: 1 };
     public workHours: WorkHoursModel = { start: '08:00', end: '20:00' };
@@ -32,7 +49,8 @@ export class CPScheduleComponent {
         enableCompactView: false,
         resources: ['Driver']
     };
-    public resourceDataSource: Object[] = [
+    /*
+    public drivers: Object[] = [
         { lastName: 'Kent', firstName: 'Clark', driverId: 1, color: '#ea7a57' },
         { lastName: 'Wayne', firstName: 'Bruce', driverId: 2, color: '#7fa900' },
         { lastName: 'Stark', firstName: 'Tony', driverId: 3, color: '#5978ee' },
@@ -44,9 +62,12 @@ export class CPScheduleComponent {
         { lastName: 'Rogers', firstName: 'Steve', driverId: 9, color: '#df5286' },
         { lastName: 'Strange', firstName: 'Stephen', driverId: 10, color: '#710193' }
     ];
-
+*/
     // only allow one driver per shift
     public allowMultiple: Boolean = false;
+
+    private colours: string[] = ['#ea7a57', '#7fa900', '#5978ee', '#fec200', '#df5286', '#00bdae', '#865fcf', '#1aaa55'];
+    safetyDrivers: ISafetyDriver[];
 
     public eventSettings: EventSettingsModel = {
         dataSource: <Object[]>extend([], shiftData, null, true),
@@ -61,15 +82,87 @@ export class CPScheduleComponent {
     };
 
     @ViewChild('scheduleObj') public scheduleObj: ScheduleComponent;
+    // END SYNCFUSION FIELDS
 
+    currentAccount: any;
+    shifts: IScheduleShift[];
+    drivers: IScheduleDriver[];
+    eventSubscriber: Subscription;
+
+    constructor(
+        private safetyDriverService: SafetyDriverService,
+        private scheduleService: ScheduleService,
+        private jhiAlertService: JhiAlertService,
+        private principal: Principal,
+        private eventManager: JhiEventManager
+    ) {}
+
+    loadAll() {
+        this.safetyDriverService
+            .query({})
+            .subscribe(
+                (res: HttpResponse<ISafetyDriver[]>) => this.parseDrivers(res.body),
+                (res: HttpErrorResponse) => this.onError(res.message)
+            );
+
+        this.scheduleService
+            .getAllFullShifts({})
+            .subscribe((res: HttpResponse<IShift[]>) => this.parseShifts(res.body), (res: HttpErrorResponse) => this.onError(res.message));
+    }
+
+    ngOnInit() {
+        this.loadAll();
+        this.principal.identity().then(account => {
+            this.currentAccount = account;
+        });
+        this.registerChangeInShifts();
+    }
+
+    ngOnDestroy() {
+        this.eventManager.destroy(this.eventSubscriber);
+    }
+
+    registerChangeInShifts() {
+        this.eventSubscriber = this.eventManager.subscribe('shiftListModification', response => this.loadAll());
+    }
+
+    private parseShifts(data: ISafetyDriver[]) {
+        console.log('myyyyyyyyy');
+        console.log(data);
+        this.drivers = data.map(
+            x =>
+                new ScheduleDriver(
+                    x.id,
+                    x.user.lastName,
+                    x.user.firstName,
+                    x.user.firstName + ' ' + x.user.lastName,
+                    this.colours[x.id % 8]
+                )
+        );
+        console.log(this.drivers);
+    }
+
+    private parseDrivers(data: IShift[]) {
+        console.log('myyyyyyyyy');
+        console.log(data);
+        this.shifts = data.map(x => new ScheduleShift(x.id, x.car.id, x.car.model, new Date(x.start), new Date(x.end), x.safetyDriver.id));
+        this.scheduleObj.refresh();
+    }
+
+    private onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
+    }
+
+    // Syncfusion: shifts in the past are read-only
     isReadOnly(endDate: Date): boolean {
         return endDate < new Date(2018, 6, 31, 0, 0);
     }
 
+    // Syncfusion: specify options for popups that open when a shift is selected
     onPopupOpen(args: PopupOpenEventArgs): void {
-        let data: { [key: string]: Object } = <{ [key: string]: Object }>args.data;
+        const data: { [key: string]: Object } = <{ [key: string]: Object }>args.data;
         if (args.type === 'QuickInfo' || args.type === 'Editor' || args.type === 'RecurrenceAlert' || args.type === 'DeleteAlert') {
-            let target: HTMLElement =
+            const target: HTMLElement =
                 args.type === 'RecurrenceAlert' || args.type === 'DeleteAlert' ? (args.data as any).element[0] : args.target;
             if (!isNullOrUndefined(target) && target.classList.contains('e-work-cells')) {
                 if (
@@ -84,16 +177,18 @@ export class CPScheduleComponent {
         }
     }
 
+    // Syncfusion: specify behaviour for user actions eventCreate and eventChange
     onActionBegin(args: ActionEventArgs): void {
         if (args.requestType === 'eventCreate' || args.requestType === 'eventChange') {
-            let data: { [key: string]: Object } = args.data as { [key: string]: Object };
-            let groupIndex: number = this.scheduleObj.eventBase.getGroupIndexFromEvent(data);
+            const data: { [key: string]: Object } = args.data as { [key: string]: Object };
+            const groupIndex: number = this.scheduleObj.eventBase.getGroupIndexFromEvent(data);
             if (!this.scheduleObj.isSlotAvailable(data.StartTime as Date, data.EndTime as Date, groupIndex as number)) {
                 args.cancel = true;
             }
         }
     }
 
+    // Syncfusion: define how cells should be rendered
     onRenderCell(args: RenderCellEventArgs): void {
         if (args.element.classList.contains('e-work-cells')) {
             if (args.date < new Date(2018, 6, 31, 0, 0)) {
@@ -102,13 +197,14 @@ export class CPScheduleComponent {
             }
         }
         if (args.elementType === 'emptyCells' && args.element.classList.contains('e-resource-left-td')) {
-            let target: HTMLElement = args.element.querySelector('.e-resource-text') as HTMLElement;
+            const target: HTMLElement = args.element.querySelector('.e-resource-text') as HTMLElement;
             target.innerHTML = '<div class="lastName">Last Name</div><div class="firstName">First Name</div><div class="driverId">ID</div>';
         }
     }
 
+    // Syncfusion: define how events should be rendered
     onEventRendered(args: EventRenderedArgs): void {
-        let data: { [key: string]: Object } = args.data;
+        const data: { [key: string]: Object } = args.data;
         if (this.isReadOnly(data.EndTime as Date)) {
             args.element.setAttribute('aria-readonly', 'true');
             args.element.classList.add('e-read-only');
