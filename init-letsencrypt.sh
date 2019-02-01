0c7521072972da3825e6de0de79608c5efefa983
Santhos Baala RS <rssanthosbaala@gmail.com>
2019-02-01 08:12:50 +0100

    Change path

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -2,7 +2,7 @@
 
 domains=(kibana.isecp.de log-collector.isecp.de dashboard.isecp.de)
 rsa_key_size=4096
-data_path="./certbot"
+data_path="./ops/certbot"
 email="rssanthosbaala@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
