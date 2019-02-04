54a3d26fae030a8b99c7f558aa89a139d6b1a794
Santhos Baala RS <rssanthosbaala@gmail.com>
2019-02-04 22:55:31 +0100

    Map reset button

--- dashboard/components/map-view/index.js
+++ dashboard/components/map-view/index.js
@@ -5,6 +5,7 @@ import Overlay from 'pigeon-overlay';
 import Measure from 'react-measure';
 
 import styles from './map-view.module.scss';
+import { Button } from 'antd';
 
 export default class MapView extends React.Component {
   static defaultProps = {
@@ -13,6 +14,7 @@ export default class MapView extends React.Component {
 
   state = {
     dimensions: { width: -1, height: -1 },
+    mapSettings: { center: [52.5126276, 13.3218814], zoom: 15 },
   }
 
   isCarInEmergency = (license) => {
@@ -22,29 +24,53 @@ export default class MapView extends React.Component {
     ) >= 0;
   }
 
+  handleResetMap = () => {
+    this.setState({ mapSettings: { center: [52.5126276, 13.3218814], zoom: 15  } });
+    this.forceUpdate();
+  }
+
+  handleMapBoundsChanged = ({ center, zoom }) => {
+    this.setState({ mapSettings: { center, zoom }});
+  }
+
   render() {
     return (
-      <Measure
-        bounds
-        onResize={contentRect => { this.setState({ dimensions: contentRect.bounds }) }}
-      >
-        {({ measureRef }) => (
-          <div ref={measureRef} className={styles.mapView}>
-            <Map center={[52.5126276, 13.3218814]} zoom={15} width={this.state.dimensions.width} height={this.state.dimensions.height}>
-              {this.props.cars.map(c => (
-                <Overlay key={c.carLicense || c.license || c.carId} anchor={c.location} offset={[0, 0]}>
-                  <svg width={25} height={25}>
-                    <rect x={0} y={0} width={25} height={25} fill={this.isCarInEmergency(c.license) ? '#f00' : '#000'} />
-                    <text x={7.5} y={17.5} fill="#fefefe">
-                      { c.carLicense || c.license || c.carId }
-                    </text>
-                  </svg>
-                </Overlay>
-              ))}
-            </Map>
-          </div>
-        )}
-      </Measure>
+      <div className={styles.mapViewContainer}>
+        <Measure
+          bounds
+          onResize={contentRect => { this.setState({ dimensions: contentRect.bounds }) }}
+        >
+          {({ measureRef }) => (
+            <div ref={measureRef} className={styles.mapView}>
+              <Map
+                center={this.state.mapSettings.center}
+                zoom={this.state.mapSettings.zoom}
+                width={this.state.dimensions.width}
+                height={this.state.dimensions.height}
+                onBoundsChanged={this.handleMapBoundsChanged}
+              >
+                {this.props.cars.map(c => (
+                  <Overlay key={c.carLicense || c.license || c.carId} anchor={c.location} offset={[0, 0]}>
+                    <svg width={25} height={25}>
+                      <rect x={0} y={0} width={25} height={25} fill={this.isCarInEmergency(c.license) ? '#f00' : '#000'} />
+                      <text x={7.5} y={17.5} fill="#fefefe">
+                        { c.carLicense || c.license || c.carId }
+                      </text>
+                    </svg>
+                  </Overlay>
+                ))}
+              </Map>
+            </div>
+          )}
+        </Measure>
+        <Button
+          className={styles.resetButton}
+          icon="compass"
+          size="large"
+          shape="circle"
+          onClick={this.handleResetMap}
+        />
+      </div>
     );
   }
 }
